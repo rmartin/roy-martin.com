1aae392bc54fb3051b35d53c7ac5675d8bd1de5a
Roy Martin <roy.martin@modop.net>
2014-09-26 23:31:13 -0700

    added nodejs and express to public stack.

---
+++ dist/server.js
@@ -0,0 +1,23 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    port = parseInt(process.env.PORT, 10) || 80;
+
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+app.listen(port);

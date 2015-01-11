c5fbbd50040ba02cda4bcaec6b28f7a258497fe1
Roy Martin <roy.martin@modop.com>
2015-01-11 01:02:17 -0800

    ensure that experiments removes homepage canvas.

--- app/scripts/modules/experiments/views/experiment.js
+++ app/scripts/modules/experiments/views/experiment.js
@@ -18,6 +18,8 @@ define(['app', 'templates'], function(App, JST) {
                 $('#header-sub-title').html('Experiments and code scribbles in web and mobile development.');
                 $('#header-attribution').html('<a href="https://commons.wikimedia.org/wiki/File:Cirrus_Clounds.jpg#mediaviewer/File:Cirrus_Clounds.jpg" target="_blank" rel="nofollow">Cirrus Clounds</a> by <a href="//commons.wikimedia.org/wiki/User:Nissim_14" title="User:Nissim 14" target="_blank" rel="nofollow">Nissim Angdembay</a>')
                 $('body').attr('class', '').addClass('experimentView');
+
+                $('.background-image canvas').remove();
             },
             updateContentPadding: function(){
                 $('.main-content').css('margin-top', $('.background-image').height() + "px");

7dae5d8dea63627d9e442d1840aad3722b0ae67d
Roy Martin <roy.martin@modop.com>
2015-01-16 23:48:40 -0800

    initial line draw addition and removal on hover.

--- app/scripts/modules/index/views/visualizer.js
+++ app/scripts/modules/index/views/visualizer.js
@@ -1,38 +1,69 @@
-define(['app', 'THREE', 'templates'], function(App, THREE, JST) {
-    App.module('IndexApp', function(IndexApp, App, Backbone, Marionette, $, _) {
+define(['app', 'THREE', 'templates'], function (App, THREE, JST) {
+    'use strict';
+    App.module('IndexApp', function (IndexApp, App, Backbone, Marionette, $, _) {
 
         //number of spheres to generate
-        var sphereCount = 20;
+        var sphereCount = 20,
+            scene,
+            camera,
+            renderer,
+            projector,
+            objects = [],
+            activeObjects = {},
+            sphereArray = [],
+            vector = new THREE.Vector3(),
+            raycaster = new THREE.Raycaster(),
+            dir = new THREE.Vector3(),
+            intersects,
+            currentIndex,
+            currentObject,
+            currentLines = [],
+            currLine,
+            currLineGeometry,
+            currLineMaterial;
 
-        var scene, camera, renderer, sphereArray,projector;
-        var objects = [];
-        var activeObjects = {};
-        var sphereArray = [];
+        IndexApp.Visualizer = (function () {
 
-        var vector = new THREE.Vector3();
-        var raycaster = new THREE.Raycaster();
-        var dir = new THREE.Vector3();
-        var intersects;
+            var getRandomNumber = function () {
+                // return Math.floor((Math.random() * 21) - 10);
+                return (Math.random() * 11) - 5;
+            }
 
-        IndexApp.Visualizer = (function(){
-
-            var getIntersections = function(){
-                if($('.background-image canvas').length !== 0){
+            var getIntersections = function () {
+                if ($('.background-image canvas').length !== 0) {
 
                     // find the unique intersections for the object based on the position of the mouse
                     vector = new THREE.Vector3();
-                    vector.set( ( window.mouseX / window.innerWidth ) * 2 - 1, - ( window.mouseY / window.innerHeight ) * 2 + 1, 0.5 ); // z = 0.5 important!
-                    vector.unproject( camera );
+                    vector.set((window.mouseX / window.innerWidth) * 2 - 1, -(window.mouseY / window.innerHeight) * 2 + 1, 0.5); // z = 0.5 important!
+                    vector.unproject(camera);
                     raycaster = new THREE.Raycaster();
-                    raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
-                    intersects = _.uniq(raycaster.intersectObjects( objects, true ), false, function(p){return p.object.id;});
+                    raycaster.set(camera.position, vector.sub(camera.position).normalize());
+                    intersects = _.uniq(raycaster.intersectObjects(objects, true), false, function (p) {
+                        return p.object.id;
+                    });
+
+
 
                     // when hovering over items, make them full circles, when hover off, revert them
-                    if(intersects.length){
-                        if(typeof activeObjects[intersects[0].object.id] === "undefined"){
+                    if (intersects.length) {
+                        if (typeof activeObjects[intersects[0].object.id] === "undefined") {
 
                             // get active object and update scale and 'pause' the animation
-                            var currentObject = sphereArray[intersects[0].object.id];
+                            currentObject = sphereArray[intersects[0].object.id];
+                            currLineMaterial = new THREE.LineBasicMaterial({
+                                color: 0x5e5e5e
+                            });
+
+                            currLineGeometry = new THREE.Geometry();
+                            currLineGeometry.vertices.push(
+                                new THREE.Vector3( currentObject.mesh.position.x, currentObject.mesh.position.y, currentObject.mesh.position.z ),
+                                new THREE.Vector3( currentObject.mesh.position.x+.5, currentObject.mesh.position.y+.5, currentObject.mesh.position.z ),
+                                new THREE.Vector3( currentObject.mesh.position.x+1, currentObject.mesh.position.y+1, currentObject.mesh.position.z )
+                            );
+
+                            currLine = new THREE.Line( currLineGeometry, currLineMaterial );
+                            scene.add(currLine);
+                            currentLines.push(currLine);
 
                             // increase the size of the circle on hover
                             currentObject.mesh.scale.x = 1.5;
@@ -45,11 +76,11 @@ define(['app', 'THREE', 'templates'], function(App, THREE, JST) {
                             currentObject.direction.z = 0;
                             activeObjects[intersects[0].object.id] = intersects[0];
                         }
-                    }else{
-                        if(activeObjects.length !== 0){
-                            for(var currentIndex in activeObjects){
+                    } else {
+                        if (activeObjects.length !== 0) {
+                            for (currentIndex in activeObjects) {
                                 // get active object and update scale and 'pause' the animation
-                                var currentObject = sphereArray[activeObjects[currentIndex].object.id];
+                                currentObject = sphereArray[activeObjects[currentIndex].object.id];
 
                                 // reset the scale
                                 currentObject.mesh.scale.x = 1;
@@ -57,9 +88,13 @@ define(['app', 'THREE', 'templates'], function(App, THREE, JST) {
                                 currentObject.mesh.scale.z = 1;
 
                                 // resume the animation
-                                currentObject.direction.x = getRandomNumber()/1000;
-                                currentObject.direction.y = getRandomNumber()/1000;
-                                currentObject.direction.z = getRandomNumber()/1000;
+                                currentObject.direction.x = getRandomNumber() / 1000;
+                                currentObject.direction.y = getRandomNumber() / 1000;
+                                currentObject.direction.z = getRandomNumber() / 1000;
+
+                                //remove the line from the scene
+                                scene.remove(currentLines.pop());
+
 
                                 // remove the item from the active hover state array
                                 delete activeObjects[currentIndex];
@@ -69,38 +104,38 @@ define(['app', 'THREE', 'templates'], function(App, THREE, JST) {
                 }
             }
 
-            var getRandomNumber = function(){
-                // return Math.floor((Math.random() * 21) - 10);
-                return (Math.random() * 11) -5;
-            }
-
-            var renderBackgroundImage = function(){
+            var renderBackgroundImage = function() {
                 var that = this;
 
-                if($('.background-image canvas').length === 0){
+                if ($('.background-image canvas').length === 0) {
                     scene = new THREE.Scene();
-                    camera = new THREE.PerspectiveCamera(45, $('body').width()/$('body').height(), 0.1, 1000);
+                    camera = new THREE.PerspectiveCamera(45, $('body').width() / $('body').height(), 0.1, 1000);
 
-                    renderer = new THREE.WebGLRenderer({alpha: true});
+                    renderer = new THREE.WebGLRenderer({
+                        alpha: true
+                    });
                     renderer.setSize($('body').width(), $('body').height());
                     $('.background-image').html(renderer.domElement);
 
-                    for(var i=0; i<=sphereCount; i++){
+                    for (var i = 0; i <= sphereCount; i++) {
 
                         // var geometry = new THREE.CircleGeometry(.5, 30);
                         // geometry.vertices.shift();
                         // geometry.dynamic = true;
                         // var material = new THREE.LineBasicMaterial({wireframe: true, color: 0x5e5e5e});
                         // var currentCircle = new THREE.Line(geometry, material);
-                        var currentObject = new THREE.Mesh(new THREE.SphereGeometry(.5, 1, 1), new THREE.MeshBasicMaterial({wireframe: true, color: 0x5e5e5e}));
-                        currentObject.position.set(getRandomNumber(),getRandomNumber(),0);
+                        var currentObject = new THREE.Mesh(new THREE.SphereGeometry(.5, 1, 1), new THREE.MeshBasicMaterial({
+                            wireframe: true,
+                            color: 0x5e5e5e
+                        }));
+                        currentObject.position.set(getRandomNumber(), getRandomNumber(), 0);
 
                         sphereArray[currentObject.id] = {
-                            'mesh' : currentObject,
+                            'mesh': currentObject,
                             'direction': {
-                                'x' : getRandomNumber()/1000,
-                                'y' : getRandomNumber()/1000,
-                                'z' : getRandomNumber()/1000,
+                                'x': getRandomNumber() / 1000,
+                                'y': getRandomNumber() / 1000,
+                                'z': getRandomNumber() / 1000,
                             }
                         }
                         scene.add(currentObject);
@@ -111,9 +146,10 @@ define(['app', 'THREE', 'templates'], function(App, THREE, JST) {
 
                     // initialize object to perform world/screen calculations
                     projector = new THREE.Projector();
+
                     var render = function() {
                         requestAnimationFrame(render);
-                        for(var currentIndex in sphereArray){
+                        for (var currentIndex in sphereArray) {
                             sphereArray[currentIndex].mesh.position.x += sphereArray[currentIndex].direction.x;
                             sphereArray[currentIndex].mesh.position.y += sphereArray[currentIndex].direction.y;
 

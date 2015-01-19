de68ebf1b73e3ef92b31fadeebbdd75bc62419fa
Roy Martin <roy.martin@modop.com>
2015-01-18 23:14:52 -0800

    Updated grunt to allow for local device testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
             options: {
                 port: grunt.option('port') || SERVER_PORT,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {

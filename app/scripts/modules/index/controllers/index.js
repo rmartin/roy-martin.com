8ae57f42050c6ef3efb885a69182473fe18dc3ee
Roy Martin <roy@roy-martin.com>
2015-02-10 23:33:19 -0800

    Refactored controller to Object class for Marionette.
    resolves #27

--- app/scripts/modules/index/controllers/index.js
+++ app/scripts/modules/index/controllers/index.js
@@ -1,7 +1,7 @@
 define(['app', 'modules/index/views/index'], function(App, IndexView) {
     App.module('IndexApp', function(IndexApp, App, Backbone, Marionette, $, _) {
 
-        IndexApp.Controller = Marionette.Controller.extend({
+        IndexApp.Controller = Marionette.Object.extend({
             index: function() {
                 var indexView = new IndexView();
                 App.main.show(indexView);

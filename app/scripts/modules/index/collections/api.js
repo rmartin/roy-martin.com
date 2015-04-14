16393197f5e163cc6266ca6aa0429d8d101eaee1
Roy Martin <roy@roy-martin.com>
2015-04-14 11:02:27 -0700

    convert modle to es6 class.

--- app/scripts/modules/index/collections/api.js
+++ app/scripts/modules/index/collections/api.js
@@ -1,14 +1,21 @@
 import Backbone from 'backbone';
 import {APIModel} from '../models/api.js';
 
-export var APICollection = Backbone.Collection.extend({
-    model: APIModel,
-    url: 'https://api-roy-martin.herokuapp.com/api/v1/all',
-    sync : function(method, collection, options) {
+export class APICollection extends Backbone.Collection{
+    model(){
+        return APIModel;
+    }
+
+    url(){
+        return 'https://api-roy-martin.herokuapp.com/api/v1/all';
+    }
+
+    sync(method, collection, options) {
         options.dataType = "jsonp";
         return Backbone.sync(method, collection, options);
-    },
-    parse: function(results){
+    }
+
+    parse(results){
         return results.data;
     }
-});
+}

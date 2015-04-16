bd51fc351a2eb02e582db1d435b972b0ab8d1015
Roy Martin <roy@roy-martin.com>
2015-04-16 10:20:34 -0700

    convert router to es6

--- app/scripts/modules/index/router.js
+++ app/scripts/modules/index/router.js
@@ -1,14 +1,13 @@
 import app from '../../app';
 import Marionette from 'backbone.marionette';
 
-export class IndexRouter extends Marionette.AppRouter {
+export class IndexRouter extends Marionette.AppRouter{
     constructor(options) {
         super(options);
-        // Marionette.AppRouter.prototype.constructor.call(this, options);
         this._getController().triggerMethod('start');
     }
-    appRoutes() {
-        return{
+    get appRoutes() {
+        return {
             '': 'index'
         }
     }

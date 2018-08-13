701cb00f18d0544417a7fedb8dbe5c2f4bea963f
Roy Martin <roy.martin@modop.com>
2018-08-12 20:09:22 -0500

    Update intro text speed and add GA tags.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Roy Martin',
-  },
-  plugins: ['gatsby-plugin-react-helmet', {
-    resolve: 'gatsby-plugin-sass',
-    options: {
-      precision: 8,
+    siteMetadata: {
+        title: 'Roy Martin',
     },
-  }],
+    plugins: ['gatsby-plugin-react-helmet', {
+        resolve: 'gatsby-plugin-sass',
+        options: {
+            precision: 8,
+        },
+    }, {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+            trackingId: "UA-58200271-1",
+            // Puts tracking script in the head instead of the body
+            head: false,
+            // Setting this parameter is optional
+            anonymize: true,
+            // Setting this parameter is also optional
+            respectDNT: true,
+            // Avoids sending pageview hits from custom paths
+            exclude: [],
+        },
+    }, ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,13 +19,6 @@ const Layout = ({ children, data }) => (
     <div>
       {children()}
     </div>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-58200271-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-58200271-1');
-    </script>
   </div>
 )
 
